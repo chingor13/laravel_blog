ed17edede71966118e2793e9426f8bf4fa560692
Jeff Ching <chingor@google.com>
2017-02-08 11:13:16 -0800

    use partial for displaying post

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,11 +8,7 @@
 
     <div class="col-sm-8 blog-main">
         @foreach ($posts as $post)
-            <div class="blog-post">
-                <h2 class="blog-post-title">{{ $post->title }}</h2>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-                {{ $post->body }}
-            </div>
+            @include ('posts.post')
         @endforeach
     </div>
 

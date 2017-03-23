2c436063e69c7672dcdece0e1026ac29c525877a
Jeff Ching <chingor@google.com>
2017-03-23 14:22:20 -0700

    Add database query listener

--- app/Providers/GoogleCloudProvider.php
+++ app/Providers/GoogleCloudProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Google\Cloud\ServiceBuilder;
 use Google\Cloud\Trace\RequestTracer;
+use Google\Cloud\Trace\Reporter\EchoReporter;
 use Google\Cloud\Trace\Reporter\TraceReporter;
 
 class GoogleCloudProvider extends ServiceProvider
@@ -20,10 +21,29 @@ class GoogleCloudProvider extends ServiceProvider
     {
         $trace = $builder->trace();
         $reporter = new TraceReporter($trace);
+        // $reporter = new EchoReporter($trace);
 
         // don't trace if we're running in the console (i.e. a php artisan command)
         if (php_sapi_name() != 'cli') {
             RequestTracer::start($trace, $reporter, ['enabled' => true, 'startTime' => LARAVEL_START]);
+            RequestTracer::retroSpan(
+                microtime(true) - LARAVEL_START,
+                [
+                    'name' => 'bootstrap'
+                ]
+            );
+
+            \Event::listen('Illuminate\Database\Events\QueryExecuted', function($event) {
+                RequestTracer::retroSpan(
+                    $event->time * 0.001,
+                    [
+                        'name' => $event->connectionName,
+                        'labels' => [
+                            'query' => $event->sql
+                        ]
+                    ]
+                );
+            });
         }
     }
 
